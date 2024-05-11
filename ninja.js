e283fa6469f0bf38932089e5dd307138f8f013f7
moisesromeropy <moises.romero2908@gmail.com>
2024-05-11 18:02:09 -0400

    [TAREA] SENSEI

--- ninja.js
+++ ninja.js
@@ -9,16 +9,30 @@ class Ninja {
         console.log(`El nombre del ninja es ${this.name}`)
     }
     showStats(){
-        console.log(`El ninja se llama ${this.name}, tiene una fuerza de
-        ${this.fuerza}, una velocidad de ${this.velocidad} y una salud de
-        ${this.salud}`)
+        console.log(`Nombre: ${this.name}, fuerza: ${this.fuerza}, velocida: ${this.velocidad}, salud: ${this.salud}`)
     }
     drinkSake(){
         this.salud = this.salud + 10;
     }
 }
-const ninja1 = new Ninja("Moises");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+class Sensei extends Ninja{
+    constructor(name){
+        super(name);
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+        this.sabiduria = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses");
+    }
+}
+const superSensei = new Sensei("Moises");
+superSensei.speakWisdom();
+superSensei.showStats();
+// const ninja1 = new Ninja("Moises");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake();
+// ninja1.showStats();
